35e70d0378359d0353038a362a69cd0deb5d7dd0
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-19 19:13:52 +0200

    Vue.js tutorial project part 1

---
+++ app.js
@@ -0,0 +1,81 @@
+new Vue ({
+  el: "#app",
+  data: {
+    showButtons: false,
+    myHealth: 100,
+    monsterHealth: 100,
+    attacks: {
+      myAttack: [],
+      monsterAttack:
+    }
+  },
+  computed: {
+    playerHealth() {
+      if (this.myHealth > 100) {
+        return {
+          width: "100%"
+        }
+      } else if (this.myHealth < 0) {
+        return {
+          width: "0%"
+        }
+      } else {
+        return {
+          width: this.myHealth + "%"
+        }
+      }
+     },
+     enemyHealth() {
+       return {
+         width: this.monsterHealth + "%"
+       }
+     }
+  },
+  methods: {
+    attack() {
+      dmg = Math.floor(Math.random()*10) + 1;
+      this.myHealth -= dmg;
+      this.myAttack.push(dmg);
+      if (this.myHealth <= 0) {
+        this.myHealth = 0;
+        alert('You died!\nPlease try again.')
+        location.reload()
+      } 
+      monsterDmg = Math.floor(Math.random()*10) +1; 
+      this.monsterHealth -= monsterDmg;
+      if (this.monsterHealth < 0) {
+        this.monsterHealth = 0;
+        alert('Congratulations!\nYou won! Play again!')
+        location.reload()
+      } 
+      console.log(this.myHealth)
+      console.log("Damage is: " + dmg)
+      console.log(this.myAttack)
+      console.log(this.monsterHealth)
+      console.log("Monster damage is: " + monsterDmg)
+    },
+    heal() {
+      healing = Math.floor(Math.random()*10) + 1;
+      this.myHealth += healing;
+      if (this.myHealth > 100) {
+        this.myHealth = 100;
+      }  
+
+      console.log('Heal is:' + healing)
+      console.log(this.myHealth)
+      
+      dmg = Math.floor(Math.random()*10) + 1;
+      this.myHealth -= dmg;
+      if (this.myHealth <= 0) {
+        this.myHealth = 0;
+        console.log('You died! Please try again')
+      } 
+      console.log(this.myHealth)
+      console.log("Damage is: " + dmg)
+    },
+    giveUp() {
+      alert('The game will be restarted');
+      location.reload();
+    }
+  }
+})
