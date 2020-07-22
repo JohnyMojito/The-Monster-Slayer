0f651de8983002f3fa73914c8dfb304561e0ced1
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-22 21:27:33 +0200

    Added some extra features to the game to check if I know how - turns out I do (except for one smalle thing!).

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ new Vue ({
   el: "#app",
   data: {
     gameOn: false,
+    enterName: true,
     myHealth: 100,
     monsterHealth: 100,
-    turns: []
+    turns: [],
+    playerName: '',
+    bgColor: 'green'
   },
   methods: {
     startGame() {
-     this.gameOn = true;
-     this.myHealth = 100;
-     this.monsterHealth = 100;
-     this.turns = [];
+      this.gameActions(true, false)
     },
     attack() {
       this.playerActions(1, 10, 'attacked')
@@ -22,21 +22,26 @@ new Vue ({
     heal() {
       var healing = this.calculateDmg(5,15);
       this.myHealth += healing;
+      this.healthActions(this.myHealth);
       this.turns.unshift({
         isPlayer: true,
-        text: 'Player healed and regained ' + healing + ' life points!'
+        text: this.playerName + ' healed and regained ' + healing + ' life points!'
       })
       if (this.myHealth > 100) {
         this.myHealth = 100;
       }  
       this.monsterAttack();
     },
-    giveUp() {
-      if(confirm('The game will be restarted!\nDo you wish to start a new game?')) {
+    restart() {
+      if (confirm('Do you want to start over?')){
         this.startGame();
-      } else {
-        this.gameOn = false;
       }
+    },
+    giveUp() {
+      if(confirm('The game will be abandoned!\nDo you wish to return to the name selection screen?')) {
+        this.gameActions(false, true)
+        this.playerName = '';
+      } 
     },
     calculateDmg(min, max) {
       return Math.max(Math.floor(Math.random()*max + 1), min)
@@ -44,6 +49,7 @@ new Vue ({
     monsterAttack() {
       var dmg = this.calculateDmg(5, 12);
       this.myHealth -= dmg;
+      this.healthActions(this.myHealth);
       this.turns.unshift({
         isPlayer: false,
         text: 'Monster attacked and dealt ' + dmg + ' points of damage!'
@@ -71,9 +77,10 @@ new Vue ({
     playerActions(min, max, action) {
       var dmg = this.calculateDmg(min, max)
       this.monsterHealth -= dmg;
+      this.healthActions(this.monsterHealth);
       this.turns.unshift({
         isPlayer: true,
-        text: 'Player ' + action + ' and dealt ' + dmg + ' points of damage!'
+        text: this.playerName + " " + action + ' and dealt ' + dmg + ' points of damage!'
       })
 
       if(this.checkWin()){
@@ -81,6 +88,27 @@ new Vue ({
       }
 
       this.monsterAttack()
+    },
+    gameActions(boolOne, boolTwo) {
+      if (this.playerName == '') {
+       alert('You forgot to enter your name!') 
+      } else {
+        this.gameOn = boolOne;
+        this.enterName = boolTwo;
+        this.myHealth = 100;
+        this.monsterHealth = 100;
+        this.turns = [];
+        this.bgColor = 'green';
+      }
+    },
+    healthActions(healthObj) {
+     if (healthObj <= 100 && healthObj > 50) {
+        this.bgColor = 'green'
+      } else if (healthObj <= 50 && healthObj > 25) {
+        this.bgColor = 'orange'
+      } else if (healthObj <= 25) {
+        this.bgColor = 'red'
+      }
     }
   }
 })
